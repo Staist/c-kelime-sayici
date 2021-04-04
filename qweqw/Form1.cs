20210b6bf06b5e59e6b18a5aa1b08bac01854184
Staist <77368284+Staist@users.noreply.github.com>
2021-04-04 21:14:31 +0300

    Add files via upload

---
+++ qweqw/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace qweqw
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] yazi = textBox1.Text.Split(' ', '/n');
+            this.label1.Text = ("Kelime sayısı : " + yazi.Length);
+
+        }
+    }
+}
